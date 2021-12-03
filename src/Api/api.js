4d2d3ef764f6a0d1d5b570262e2e10a4e4aa77ad
GuilhermeKageyama <guiproc@gmail.com>
2021-12-03 10:10:34 -0300

    env_axios

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,4 +1,20 @@
 import axios from "axios";
-const api = axios.create(process.env.BASE_URL);
+import { useState, useEffect } from "react";
 
-export default api;
+const Data = () => {
+  const axios = require("axios");
+  const api = axios.create(process.env.BASE_URL);
+  // const api = axios.create(
+  //   `http://ec2-18-206-40-162.compute-1.amazonaws.com:8080/`
+  // );
+  const [responseData, setResponseData] = useState([]);
+  useEffect(() => {
+    api
+      .get(`${api}/products/categories`)
+      .then((res) => setResponseData(res.data))
+      .catch((err) => console.log(err));
+  }, [api]);
+
+  return <>{console.log(responseData)}</>;
+};
+export default Data;
--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import "./App.scss";
-<<<<<<< HEAD
 import RouteList from "./Routes";
 import Home from "./Pages/Home";
 import "dotenv/config";
-=======
-import  RouteList from './Routes/';
->>>>>>> 2096ddbf880d14de562b294dc7ded0e17b7862bc
 function App() {
   return (
       <RouteList />
