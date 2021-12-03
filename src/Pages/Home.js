c80fd57069228e133f24bd6b51f2dead517ced49
Rener <rener.almeida@gmail.com>
2021-12-03 16:43:33 -0300

    footer

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,13 @@
 import Header from "../Components/Header/Header";
 import { Carousel } from "../Components/Carousel";
+import Footer from "../Components/Footer/Footer";
+
 const Home = () => {
   return (
     <>
       <Header />
       <Carousel />
+	  <Footer />
       
     </>
   );
