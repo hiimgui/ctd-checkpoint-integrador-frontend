e98ac90bf93716035c7f80a975ef05229b611c3b
GuilhermeKageyama <guiproc@gmail.com>
2021-12-06 11:56:42 -0300

    carousel arrows

--- src/Components/Carousel/index.jsx
+++ src/Components/Carousel/index.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import Slider from "react-slick";
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 import { Container } from "react-bootstrap";
-import { FaChevronLeft, FaChevronRight, FaShoppingCart } from 'react-icons/fa';
+import { FaChevronLeft, FaChevronRight, FaShoppingCart } from "react-icons/fa";
 
-import api from '../../Api/api';
+import api from "../../Api/api";
 
 import { Link } from "react-router-dom";
 
@@ -13,95 +13,88 @@ import "../../../node_modules/slick-carousel/slick/slick.css";
 import "../../../node_modules/slick-carousel/slick/slick-theme.css";
 import "./styles.scss";
 
-
 export const Carousel = () => {
+  const [data, setData] = React.useState([]);
 
-    const [data, setData] = React.useState([]);
+  useEffect(() => {
+    api
+      .get(`${process.env.REACT_APP_API_URL}/products`)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    useEffect(() => {
-        api.get(`${process.env.REACT_APP_API_URL}/products`).then(response => {
-            setData(response.data);
-        }).catch(error => {
-            console.log(error);
-        }
-        );
-    }, []);
+  const settings = {
+    className: "carousel",
+    dots: true,
+    infinite: true,
+    speed: 500,
+    autoplaySpeed: 3000,
+    autoplay: true,
+    pauseOnHover: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
+    customPaging: function (i) {
+      return <div className="dots-simbol"></div>;
+    },
+  };
 
+  const getRandomItens = (items, number) => {
+    items.sort(function () {
+      return 0.5 - Math.random();
+    });
+    return items.slice(0, number);
+  };
 
-    const settings = {
-        className: "carousel",
-        dots: true,
-        infinite: true,
-        speed: 500,
-        autoplaySpeed: 3000,
-        autoplay: true,
-        pauseOnHover: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        arrows: true,
-        prevArrow: <NextArrow />,
-        nextArrow: <PrevArrow />,
-        customPaging: function(i) {
-            return (
-              <div className="dots-simbol">
-                </div>
-            );
-          },
-    };
+  return (
+    <Container fluid="md">
+      <div className="mt-4 mb-1 d-flex flex-row align-items-center bd-highlight text-uppercase fw-bold ">
+        <h3 className="flex-fill text-center">Destaques</h3>
+        <Link to="/" className="align-self-end  me-3">
+          Ver todas ofertas
+        </Link>
+      </div>
 
-    const getRandomItens = (items,number) => {
-        items.sort(function() {return 0.5 - Math.random()})
-        return items.slice(0,number);
-    }
-        
-
-    return (
-        <Container fluid="md">
-        <div className="mt-4 mb-1 d-flex flex-row align-items-center bd-highlight text-uppercase fw-bold ">
-        <h3 className="flex-fill text-center">Destaques</h3> 
-        <Link to="/" className="align-self-end  me-3">Ver todas ofertas</Link>
-        </div>          
-        
-            <Slider {...settings}>
-
-                {getRandomItens(data, 9).map(product =>
-                    <CardCarousel key={product.id} {...product} />
-                )}
-
-            </Slider>
-
-        </Container>
-    );
-}
-
+      <Slider {...settings}>
+        {getRandomItens(data, 9).map((product) => (
+          <CardCarousel key={product.id} {...product} />
+        ))}
+      </Slider>
+    </Container>
+  );
+};
 
 const CardCarousel = ({ title, price, category, description, image }) => {
-    return (
-        <Card>
-            <Card.Img variant="top" src={image} />
-            <Card.Body className="flex-fill">
-                <Card.Title className="text-center">{title}</Card.Title>
-                <Card.Text className="text-muted" >
-                    {description}
-                </Card.Text>
-                <Card.Subtitle as="h3">
-                    <span >{"R$ " + price.toString()}</span>
-                    <Button variant="outline-primary" size="sm">Saiba mais</Button>
-                </Card.Subtitle>
-            </Card.Body>
-            <Button variant="primary" className="text-uppercase"><FaShoppingCart className="me-2" /> Comprar</Button>
-        </Card>
-    );
-}
+  return (
+    <Card>
+      <Card.Img variant="top" src={image} />
+      <Card.Body className="flex-fill">
+        <Card.Title className="text-center">{title}</Card.Title>
+        <Card.Text className="text-muted">{description}</Card.Text>
+        <Card.Subtitle as="h3">
+          <span>{"R$ " + price.toString()}</span>
+          <Button variant="outline-primary" size="sm">
+            Saiba mais
+          </Button>
+        </Card.Subtitle>
+      </Card.Body>
+      <Button variant="primary" className="text-uppercase">
+        <FaShoppingCart className="me-2" /> Comprar
+      </Button>
+    </Card>
+  );
+};
 
 function NextArrow({ currentSlide, slideCount, ...props }) {
-    return (
-        <FaChevronLeft {...props} />
-    );
+  return <FaChevronRight {...props} />;
 }
 
 function PrevArrow({ currentSlide, slideCount, ...props }) {
-    return (
-        <FaChevronRight {...props} />
-    );
+  return <FaChevronLeft {...props} />;
 }
